263a7ffdfc4d8f2be5d01566abc040f1e4b470da
Henrik Christiansen <henpia@gmail.com>
2018-04-04 14:40:38 +0200

    Intiel opsætning af projekt.

---
+++ Bibliotek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bibliotek.Startup))]
+namespace Bibliotek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
